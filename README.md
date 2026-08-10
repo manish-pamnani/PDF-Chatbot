0e6d8e0d00c805f81a3e966dbd7e645a6113edff
Manish Pamnani <manishpamnani169@gmail.com>
2026-08-10 10:51:03 +0530

    Add Express server and API endpoints for chatbot functionality
    - Integrated Express and body-parser to handle API requests.
    - Created a health check endpoint and a chat endpoint for real-time answers from the LLM.
    - Updated package.json and package-lock.json to include new dependencies.
    - Added README.md with installation instructions and prerequisites for the project.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Prereuisites
+
+- Chroma DB
+- LangChain
+- Ollama (llama3.2:3b)
+- Docker
+
+# Installation
+
+- Install the dependecies via:
+
+```
+npm install
+```
+
+- Start the Chroma Server on CLI using:
+
+```
+npx chroma run --path ./PDF-ChatBot
+```
+
+- Start the Script using:
+
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
 import { ChromaClient } from "chromadb";
 import { DefaultEmbeddingFunction } from "@chroma-core/default-embed";
 import ollama from "ollama";
+import express from "express";
+import bodyParser from "body-parser";
 
 // Load a PDF
 async function loadPDFPages(filePath) {
@@ -55,31 +57,91 @@ await collection.add({
 });
 
 // Get all the records in collection to be given to ollama for full context
-const results = await collection.get({
-    include: ["documents", "metadatas"]
+// This is split-ingestion from querying: PDF-parsing, chunking, and embedding into chromadb should run once.
+// This is not as efficient in the real world so I am moving to the Semantic search (i.e. getting answers while chatting with a chatbot)
+// const results = await collection.get({
+//     include: ["documents", "metadatas"]
+// })
+
+// // this will still give us the answer in object form which is not understandable by user, so we need to pass the stored values to Ollama for chatbot type experience (which we will do in next step).
+// // console.log(results);
+
+// // Pass the stored values in chromadb as the context to Ollama for chatbot type experience.
+// const context = results.documents.join("\n\n");
+
+// const response = await ollama.chat({
+//     model: "llama3.2:3b",
+//     messages: [
+//         {
+//             role: "system",
+//             content: "You are a helpful assistant. Answer the user's question using only the provided context."
+//         },
+//         {
+//             role: "user",
+//             content: `Context:\n${context}\n\nQuestion: What is someone misuses the policy?`
+//         }
+//     ]
+// })
+
+// console.log(response.message.content);
+
+
+
+
+
+
+
+
+
+
+
+
+// Make a Express server for an API Endpoint
+const app = express();
+const PORT = 5000;
+const router = express.Router();
+
+// API Endpoints for checking the health of the ChatBot
+router.get('/health', (req, res) => {
+    res.status(200).send('Health OK');
+});
+
+// API Endpoint for getting real time answers from the LLM:
+router.post('/chat',async (req, res) => {
+    console.log(req.body.question);
+
+    // Query the Model with only the most relevant chunk.
+    const results = await collection.query({
+        queryTexts: [`${req.body.question}`]
+    });
+
+    const context = results.documents.join("\n\n");
+
+    const response = await ollama.chat({
+        model: "llama3.2:3b",
+        messages: [
+            {
+                role: "system",
+                content: "You are a helpful assistant. Answer the user's question using only the provided context."
+            },
+            {
+                role: "user",
+                content: `Content:\n${context}\n\nQuestion: ${req.body.question}?`
+            }
+        ]
+    })
+
+    res.status(200).send({
+        answer: response.message.content
+    })
 })
 
-// this will still give us the answer in object form which is not understandable by user, so we need to pass the stored values to Ollama for chatbot type experience (which we will do in next step).
-// console.log(results);
+export default router
 
-// Pass the stored values in chromadb as the context to Ollama for chatbot type experience.
-const context = results.documents.join("\n\n");
+app.use(bodyParser.json());
+app.use(router);
 
-const response = await ollama.chat({
-    model: "llama3.2:3b",
-    messages: [
-        {
-            role: "system",
-            content: "You are a helpful assistant. Answer the user's question using only the provided context."
-        },
-        {
-            role: "user",
-            content: `Context:\n${context}\n\nQuestion: What is someone misuses the policy?`
-        }
-    ]
-})
-
-console.log(response.message.content);
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
 
 // Query from the db
 // const results = await collection.query({
